91286980187ee1720713baf33ed036f501418a9d
sonproject123 <sonproject123@gmail.com>
2024-12-12 07:08:19 +0900

    맵 생성 11

--- Cielle/Assets/Scripts/Map Generator/Map_Generator_Generic.cs
+++ Cielle/Assets/Scripts/Map Generator/Map_Generator_Generic.cs
@@ -6,7 +6,12 @@ using UnityEngine;
 
 public class Map_Generator_Generic : MonoBehaviour {
     [SerializeField] protected string stage;
-    [SerializeField] List<RoomTemplate> roomTemplates = new List<RoomTemplate>();
+    [SerializeField] RoomTemplate start = null;
+    [SerializeField] List<RoomTemplate> rooms = new List<RoomTemplate>();
+    [SerializeField] List<RoomTemplate> monsters = new List<RoomTemplate>();
+    [SerializeField] List<RoomTemplate> corridors = new List<RoomTemplate>();
+    [SerializeField] List<RoomTemplate> goals = new List<RoomTemplate>();
+    [SerializeField] List<RoomTemplate> deadends = new List<RoomTemplate>();
 
     protected virtual void Awake() {
         string folderPath = "Assets/Resources/Rooms/" + stage;
@@ -16,14 +21,33 @@ public class Map_Generator_Generic : MonoBehaviour {
             return;
         }
 
-        string[] templateGUIDs = AssetDatabase.FindAssets("t:RT", new[] { folderPath });
+        string[] templateGUIDs = AssetDatabase.FindAssets("t:RoomTemplate", new[] { folderPath });
 
         foreach(string guid in templateGUIDs) {
             string templatePath = AssetDatabase.GUIDToAssetPath(guid);
             RoomTemplate roomTemplate = AssetDatabase.LoadAssetAtPath<RoomTemplate>(templatePath);
 
             if (roomTemplate != null) {
-                roomTemplates.Add(roomTemplate);
+                switch (roomTemplate.type) {
+                    case "Start":
+                        start = roomTemplate;
+                        break;
+                    case "Room":
+                        rooms.Add(roomTemplate);
+                        break;
+                    case "Corridor":
+                        corridors.Add(roomTemplate);
+                        break;
+                    case "Monster":
+                        monsters.Add(roomTemplate);
+                        break;
+                    case "DeadEnd":
+                        deadends.Add(roomTemplate);
+                        break;
+                    case "Goal":
+                        rooms.Add(roomTemplate);
+                        break;
+                }
             }
         }
     }
